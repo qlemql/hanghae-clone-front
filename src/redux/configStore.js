c40175dbc366aeef1332b1a54752f22a8dfba1ac
qlemql <busgamer@naver.com>
2021-07-17 11:00:11 +0900

    [add] response.json 생성 및 configStore update
    modules file 생성

---
+++ src/redux/configStore.js
@@ -0,0 +1,35 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
+
+import user from "./modules/user";
+import comment from "./modules/comment";
+
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  user: user,
+  comment: comment,
+  router: connectRouter(history),
+});
+
+const middlewares = [thunk.withExtraArgument({ history: history })];
+
+const env = process.env.NODE_ENV;
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+let store = createStore(rootReducer, enhancer);
+
+export default store;
